f97685f58404b82e2733fed340b345fcf14e17a3
JP Ngabonziza <jeanpierren@gmail.com>
2022-12-11 18:00:34 +0200

    Fixed footer layout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
-
+import './Footer.scss';
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrival</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do euusmod
+            incididunt ut labore et dolore. lorem ipsum dolor sit amet conse ctetur adipisicing elit.
+          </span>
+        </div>
+        <div className="item">
+          <h1>Contact</h1>
+          <span>
+            Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do euusmod
+            incididunt ut labore et dolore. Lorem ipsum dolor sit amet conse ctetur adipisicing elit.
+          </span>
+        </div>
+      </div>
+      
+      <div className="bottom">
+          <div className="left">
+            <span className="logo">LAMASTORE</span>
+            <span className="copyright">&#169;copyright 2022. All rights reserved.</span>
+          </div>
+          <div className="right">
+            <img src="/img/payment.png" alt=""/>
+          </div>
+      </div>
+    </div>
   )
 }
 
