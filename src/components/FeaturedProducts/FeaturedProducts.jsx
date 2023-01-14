6a4ef273c28fc1ddb551e294eeb27ae7bfede2c1
JP Ngabonziza <jeanpierren@gmail.com>
2023-01-14 23:54:12 +0200

    Added Filtering option using strapi

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -11,7 +11,7 @@ export const FeaturedProducts = ({type}) => {
         const fetchData = async () => {
             try {
                 const res = await axios.get(
-                process.env.REACT_APP_API_URL + "/products?populate=*",
+                process.env.REACT_APP_API_URL + `/products?populate=*&[filters][type][$eq]=${type}`,
                 {
                     headers: {
                         Authorization: "bearer " + process.env.REACT_APP_API_TOKEN,
