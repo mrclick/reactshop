02ce0386c2ec630b36037014656239b6ebfb5fb3
JP Ngabonziza <jeanpierren@gmail.com>
2023-01-14 23:45:55 +0200

    Fetching trending & featured products

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,43 +1,32 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react';
 import Card from '../Card/Card';
-import "./FeaturedProducts.scss"
+import "./FeaturedProducts.scss";
+import axios from "axios";
 
 export const FeaturedProducts = ({type}) => {
-    
-    const data = [
-        {
-            id:1,
-            img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress=tinysrgb&w1600",
-            img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress=tinysrgb&w1600",
-            title: "Long Sleeve Graphic T-shirt",
-            isNew:true,
-            oldPrice:19,
-            price:12,
-        },
-        {
-            id:2,
-            img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress=tinysrgb&w1600",
-            title: "Coat",
-            isNew:true,
-            oldPrice:19,
-            price:12,
-        },
-        {
-            id:3,
-            img: "https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress=tinysrgb&w1600",
-            title: "Skirt",
-            oldPrice:19,
-            price:12,
-        },
-        {
-            id:4,
-            img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress=tinysrgb&w1600",
-            title: "Hat",
-            oldPrice:19,
-            price:12,
-        },
-    ];
 
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.get(
+                process.env.REACT_APP_API_URL + "/products?populate=*",
+                {
+                    headers: {
+                        Authorization: "bearer " + process.env.REACT_APP_API_TOKEN,
+                    },
+                }
+              );
+              setData(res.data.data)
+              
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        fetchData(); 
+    }, []);
+    console.log(data)
   return (
     <div className='featuredProducts'>
         <div className="top">
